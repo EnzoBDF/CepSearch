99c3b2e87ecd866adfb40449123245007aa78d9d
Bigborg <100381400+EnzoBDF@users.noreply.github.com>
2023-06-26 10:09:08 -0300

    Add files via upload

---
+++ BuscaCEP/script.js
@@ -0,0 +1,17 @@
+function buscaCep(){
+    let cep = document.getElementById("cep").value
+    let cidade = document.getElementById("Cidade")
+    let estado = document.getElementById("Estado")
+    let bairro = document.getElementById("Bairro")
+    let logradouro = document.getElementById("Logradouro")
+    let cepValido = cep.substr(0,5)+ "-" + cep.substr(5)+".json"
+    let url = "https://cdn.apicep.com/file/apicep/"+cepValido
+    fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                cidade.value = "    "+data.city;
+                estado.value = "    "+data.state;
+                bairro.value = "    "+data.district;
+                logradouro.value = "    "+data.address;
+            })
+}
